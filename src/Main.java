ff8dbf396d9e24e05d698ac67463398175c124c1
NataBarsu <khvorostukhina.n@yandex.ru>
2022-06-23 23:00:40 +0300

    Files added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_500;
+        int bonus=20;
+        System.out.println ("Начисленные мили =" + (price/bonus));
+    }
+}
